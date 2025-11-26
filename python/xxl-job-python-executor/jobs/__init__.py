961e9fc412ebadbaee17b5081a6a7f4a192e616f
ckf10000 <ckf10000@sina.com>
2025-11-26 11:34:34 +0800

    1. 修改readme

--- python/xxl-job-python-executor/jobs/__init__.py
+++ python/xxl-job-python-executor/jobs/__init__.py
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------------------
 # ProjectName:  base-mirrors
 # FileName:     __init__.py
-# Description:  TODO
+# Description:  测试任务包
 # Author:       ASUS
 # CreateDate:   2025/11/26
 # Copyright ©2011-2025. Hunan xxxxxxx Company limited. All rights reserved.
